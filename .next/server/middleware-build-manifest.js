4e2885720eff28d7e474f0e7d1f2af4ad2943018
SY NEA GUY CEDRIC <syneaguycedric@gmail.com>
2025-09-15 22:27:24 +0000

    suppression fichier index.html

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -29,11 +29,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/biens/[id]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/biens/[id].js"
     ]
   },
   "ampFirstPages": []
